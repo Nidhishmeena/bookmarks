20419d234c45b8a47c58decb18e85e5f4a84ff49
nid <nid@ubuntu.(none)>
2014-01-07 06:15:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nid::Application.config.secret_key_base = '86ca705df2b1a38b8666d65574abafdd60f1448c2b8252f24d50d7e80a5e95536fd0a64d0547b273458f733c874c885ae1d669b72acffa8cbed39f4a9a512293'
